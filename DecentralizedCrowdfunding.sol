86739fc1e3f34949269812b1754e1845af579312
sampatsinghshekhawat <sampatsinghs792@gmail.com>
2025-03-17 15:54:03 +0530

    Update DecentralizedCrowdfunding.sol

--- DecentralizedCrowdfunding.sol
+++ DecentralizedCrowdfunding.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract DecentralizedCrowdfunding {
+    struct Project {
+        string title;
+        string description;
+        address creator;
+    }
+
+    Project public project;
+
+    constructor(string memory _title, string memory _description) {
+        project = Project({
+            title: _title,
+            description: _description,
+            creator: msg.sender
+        });
+    }
+
+    function getProjectDetails() public view returns (string memory, string memory, address) {
+        return (project.title, project.description, project.creator);
+    }
+}
 
